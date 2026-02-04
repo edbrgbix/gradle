scm-repo-mirror/gradle_gradle

Complete internal-distribution-testing split
Move some static system property based utilities from GradleContextualExecuter to IntegrationTestBuildContext to avoid pulling in the whole executer infrastructure
into the distribution testing project
Ajdust dependencies as suggested by projectHealth

testing/internal-distribution-testing/build.gradle.kts
@@ -1,5 +1,26 @@
+import gradlebuild.integrationtests.tasks.GenerateLanguageAnnotations
+import java.util.Properties
+
 plugins {
-    id("gradlebuild.distribution.api-java")
+    id("gradlebuild.internal.java")
+}
+
+description = "Collection of test fixtures for distribution tests, internal use only"
+
+jvmCompile {
+    compilations {
+        named("main") {
+            // These test fixtures are used by the tooling API tests, which still run on JVM 8
+            targetJvmVersion = 8
+        }
+    }
+}
+
+sourceSets {
+    main {
+        // Incremental Groovy joint-compilation doesn't work with the Error Prone annotation processor
+        errorprone.enabled = false
+    }
 }
 
 dependencies {
@@ -12,25 +33,25 @@ dependencies {
     api(projects.jvmServices)
     api(projects.persistentCache)
     api(projects.stdlibJavaExtensions)
-    api("com.google.code.findbugs:jsr305")
-    api("com.google.guava:guava")
-    api("javax.servlet:javax.servlet-api")
-    api("jcifs:jcifs")
-    api("junit:junit")
-    api("org.apache.groovy:groovy")
-    api("org.apache.groovy:groovy-xml")
-    api("org.eclipse.jetty:jetty-security")
-    api("org.eclipse.jetty:jetty-server")
-    api("org.eclipse.jetty:jetty-util")
-    api("org.gradle.exemplar:samples-check")
-    api("org.hamcrest:hamcrest")
-    api("org.jetbrains:annotations")
-    api("org.slf4j:slf4j-api")
-    api("org.spockframework:spock-core")
+    api(libs.groovy)
+    api(libs.groovyXml)
+    api(libs.gson)
+    api(libs.guava)
+    api(libs.hamcrest)
+    api(libs.jettySecurity)
+    api(libs.jettyServer)
+    api(libs.jettyUtil)
+    api(libs.jsr305)
+    api(libs.junit)
+    api(libs.samplesCheck)
+    api(libs.servletApi)
+    api(libs.slf4jApi)
+    api(libs.spock)
 
     implementation(projects.baseServicesGroovy)
+    implementation(projects.buildProcessServices)
     implementation(projects.buildOperations)
-    implementation(projects.core)
+    implementation(projects.clientServices)
     implementation(projects.daemonLogging)
     implementation(projects.dependencyManagement)
     implementation(projects.enterpriseLogging)
@@ -47,15 +68,82 @@ dependencies {
     implementation(projects.time)
     implementation(projects.toolingApi)
     implementation(projects.wrapperShared)
-    implementation("com.google.code.gson:gson")
-    implementation("commons-io:commons-io")
-    implementation("net.rubygrapefruit:ansi-control-sequence-util")
-    implementation("net.rubygrapefruit:native-platform")
-    implementation("org.apache.commons:commons-compress")
-    implementation("org.apache.commons:commons-lang3")
-    implementation("org.apache.groovy:groovy-json")
-    implementation("org.apache.httpcomponents:httpcore")
-    implementation("org.apache.ivy:ivy")
-    implementation("org.eclipse.jetty:jetty-http")
-    implementation("org.jspecify:jspecify")
+    implementation(testFixtures(projects.core))
+    implementation(testFixtures(projects.enterpriseLogging))
+    implementation(libs.ansiControlSequenceUtil)
+    implementation(libs.commonsCompress)
+    implementation(libs.commonsIo)
+    implementation(libs.commonsLang)
+    implementation(libs.groovyJson)
+    implementation(libs.httpcore)
+    implementation(libs.ivy)
+    implementation(libs.jcifs)
+    implementation(libs.jetty)
+    implementation(libs.nativePlatform)
+
+    compileOnly(libs.jetbrainsAnnotations)
+    compileOnly(libs.jspecify)
+
+    integTestDistributionRuntimeOnly(projects.distributionsCore)
+}
+
+val prepareVersionsInfo = tasks.register<PrepareVersionsInfo>("prepareVersionsInfo") {
+    destFile = layout.buildDirectory.file("generated-resources/all-released-versions/all-released-versions.properties")
+    versions = gradleModule.identity.releasedVersions.map {
+        it.allPreviousVersions.joinToString(" ") { it.version }
+    }
+    mostRecent = gradleModule.identity.releasedVersions.map { it.mostRecentRelease.version }
+    mostRecentSnapshot = gradleModule.identity.releasedVersions.map { it.mostRecentSnapshot.version }
+}
+
+val copyTestedVersionsInfo by tasks.registering(Copy::class) {
+    from(isolated.rootProject.projectDirectory.file("gradle/dependency-management/agp-versions.properties"))
+    from(isolated.rootProject.projectDirectory.file("gradle/dependency-management/kotlin-versions.properties"))
+    from(isolated.rootProject.projectDirectory.file("gradle/dependency-management/smoke-tested-plugins.properties"))
+    into(layout.buildDirectory.dir("generated-resources/tested-versions"))
+}
+
+val generateLanguageAnnotations by tasks.registering(GenerateLanguageAnnotations::class) {
+    classpath.from(configurations.integTestDistributionRuntimeClasspath)
+    packageName = "org.gradle.integtests.fixtures"
+    destDir = layout.buildDirectory.dir("generated/sources/language-annotations/groovy/main")
+}
+
+sourceSets.main {
+    groovy.srcDir(generateLanguageAnnotations.flatMap { it.destDir })
+    output.dir(prepareVersionsInfo.map { it.destFile.get().asFile.parentFile })
+    output.dir(copyTestedVersionsInfo)
+}
+
+@CacheableTask
+abstract class PrepareVersionsInfo : DefaultTask() {
+
+    @get:OutputFile
+    abstract val destFile: RegularFileProperty
+
+    @get:Input
+    abstract val mostRecent: Property<String>
+
+    @get:Input
+    abstract val versions: Property<String>
+
+    @get:Input
+    abstract val mostRecentSnapshot: Property<String>
+
+    @TaskAction
+    fun prepareVersions() {
+        val properties = Properties()
+        properties["mostRecent"] = mostRecent.get()
+        properties["mostRecentSnapshot"] = mostRecentSnapshot.get()
+        properties["versions"] = versions.get()
+        gradlebuild.basics.util.ReproduciblePropertiesWriter.store(properties, destFile.get().asFile)
+    }
+}
+
+packageCycles {
+    excludePatterns.add("org/gradle/**")
+}
+
+tasks.isolatedProjectsIntegTest {
+    enabled = false
 }

edbrgbix/gradle

testing/internal-distribution-testing/build.gradle.kts
@@ -1,5 +1,26 @@
+import gradlebuild.integrationtests.tasks.GenerateLanguageAnnotations
+import java.util.Properties
+
 plugins {
-    id("gradlebuild.distribution.api-java")
+    id("gradlebuild.internal.java")
+}
+
+description = "Collection of test fixtures for distribution tests, internal use only"
+
+jvmCompile {
+    compilations {
+        named("main") {
+            // These test fixtures are used by the tooling API tests, which still run on JVM 8
+            targetJvmVersion = 8
+        }
+    }
+}
+
+sourceSets {
+    main {
+        // Incremental Groovy joint-compilation doesn't work with the Error Prone annotation processor
+        errorprone.enabled = false
+    }
 }
 
 dependencies {
@@ -12,25 +33,25 @@ dependencies {
     api(projects.jvmServices)
     api(projects.persistentCache)
     api(projects.stdlibJavaExtensions)
-    api("com.google.code.findbugs:jsr305")
-    api("com.google.guava:guava")
-    api("javax.servlet:javax.servlet-api")
-    api("jcifs:jcifs")
-    api("junit:junit")
-    api("org.apache.groovy:groovy")
-    api("org.apache.groovy:groovy-xml")
-    api("org.eclipse.jetty:jetty-security")
-    api("org.eclipse.jetty:jetty-server")
-    api("org.eclipse.jetty:jetty-util")
-    api("org.gradle.exemplar:samples-check")
-    api("org.hamcrest:hamcrest")
-    api("org.jetbrains:annotations")
-    api("org.slf4j:slf4j-api")
-    api("org.spockframework:spock-core")
+    api(libs.groovy)
+    api(libs.groovyXml)
+    api(libs.gson)
+    api(libs.guava)
+    api(testLibs.hamcrest)
+    api(testLibs.jettySecurity)
+    api(testLibs.jettyServer)
+    api(testLibs.jettyUtil)
+    api(libs.jsr305)
+    api(testLibs.junit)
+    api(testLibs.samplesCheck)
+    api(testLibs.servletApi)
+    api(libs.slf4jApi)
+    api(testLibs.spock)
 
     implementation(projects.baseServicesGroovy)
+    implementation(projects.buildProcessServices)
     implementation(projects.buildOperations)
-    implementation(projects.core)
+    implementation(projects.clientServices)
     implementation(projects.daemonLogging)
     implementation(projects.dependencyManagement)
     implementation(projects.enterpriseLogging)
@@ -47,15 +68,82 @@ dependencies {
     implementation(projects.time)
     implementation(projects.toolingApi)
     implementation(projects.wrapperShared)
-    implementation("com.google.code.gson:gson")
-    implementation("commons-io:commons-io")
-    implementation("net.rubygrapefruit:ansi-control-sequence-util")
-    implementation("net.rubygrapefruit:native-platform")
-    implementation("org.apache.commons:commons-compress")
-    implementation("org.apache.commons:commons-lang3")
-    implementation("org.apache.groovy:groovy-json")
-    implementation("org.apache.httpcomponents:httpcore")
-    implementation("org.apache.ivy:ivy")
-    implementation("org.eclipse.jetty:jetty-http")
-    implementation("org.jspecify:jspecify")
+    implementation(testFixtures(projects.core))
+    implementation(testFixtures(projects.enterpriseLogging))
+    implementation(testLibs.ansiControlSequenceUtil)
+    implementation(libs.commonsCompress)
+    implementation(libs.commonsIo)
+    implementation(libs.commonsLang)
+    implementation(libs.groovyJson)
+    implementation(libs.httpcore)
+    implementation(libs.ivy)
+    implementation(libs.jcifs)
+    implementation(testLibs.jetty)
+    implementation(libs.nativePlatform)
+
+    compileOnly(libs.jetbrainsAnnotations)
+    compileOnly(libs.jspecify)
+
+    integTestDistributionRuntimeOnly(projects.distributionsCore)
+}
+
+val prepareVersionsInfo = tasks.register<PrepareVersionsInfo>("prepareVersionsInfo") {
+    destFile = layout.buildDirectory.file("generated-resources/all-released-versions/all-released-versions.properties")
+    versions = gradleModule.identity.releasedVersions.map {
+        it.allPreviousVersions.joinToString(" ") { it.version }
+    }
+    mostRecent = gradleModule.identity.releasedVersions.map { it.mostRecentRelease.version }
+    mostRecentSnapshot = gradleModule.identity.releasedVersions.map { it.mostRecentSnapshot.version }
+}
+
+val copyTestedVersionsInfo by tasks.registering(Copy::class) {
+    from(isolated.rootProject.projectDirectory.file("gradle/dependency-management/agp-versions.properties"))
+    from(isolated.rootProject.projectDirectory.file("gradle/dependency-management/kotlin-versions.properties"))
+    from(isolated.rootProject.projectDirectory.file("gradle/dependency-management/smoke-tested-plugins.properties"))
+    into(layout.buildDirectory.dir("generated-resources/tested-versions"))
+}
+
+val generateLanguageAnnotations by tasks.registering(GenerateLanguageAnnotations::class) {
+    classpath.from(configurations.integTestDistributionRuntimeClasspath)
+    packageName = "org.gradle.integtests.fixtures"
+    destDir = layout.buildDirectory.dir("generated/sources/language-annotations/groovy/main")
+}
+
+sourceSets.main {
+    groovy.srcDir(generateLanguageAnnotations.flatMap { it.destDir })
+    output.dir(prepareVersionsInfo.map { it.destFile.get().asFile.parentFile })
+    output.dir(copyTestedVersionsInfo)
+}
+
+@CacheableTask
+abstract class PrepareVersionsInfo : DefaultTask() {
+
+    @get:OutputFile
+    abstract val destFile: RegularFileProperty
+
+    @get:Input
+    abstract val mostRecent: Property<String>
+
+    @get:Input
+    abstract val versions: Property<String>
+
+    @get:Input
+    abstract val mostRecentSnapshot: Property<String>
+
+    @TaskAction
+    fun prepareVersions() {
+        val properties = Properties()
+        properties["mostRecent"] = mostRecent.get()
+        properties["mostRecentSnapshot"] = mostRecentSnapshot.get()
+        properties["versions"] = versions.get()
+        gradlebuild.basics.util.ReproduciblePropertiesWriter.store(properties, destFile.get().asFile)
+    }
+}
+
+packageCycles {
+    excludePatterns.add("org/gradle/**")
+}
+
+tasks.isolatedProjectsIntegTest {
+    enabled = false
 }